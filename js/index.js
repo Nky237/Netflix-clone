9042f35fd1bd714142208599ece4b1e1ae239c64
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-01-12 11:17:58 +0100

    added accordion

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
-// RESPONSIVE NAV MENU
-const Navy = document.querySelector('.nav-menu')
-const Ham = document.querySelector('.harmburger')
-const Linky = document.querySelectorAll('.nav-link')
+const accordionTitles = document.querySelectorAll(".accord");
 
-	Ham.addEventListener("click", ()=>{
-	Navy.classList.toggle("active")
-	Ham.classList.toggle("active")
-})
-Linky.forEach(n=> n.addEventListener('click', ()=>{
-	Navy.classList.remove("active")
-	Ham.classList.remove("active")
-}))
+accordionTitles.forEach((accordionTitle) => {
+  accordionTitle.addEventListener("click", () => {
+    if (accordionTitle.classList.contains("is-open")) {
+      accordionTitle.classList.remove("is-open");
+    } else {
+      const accordionTitlesWithIsOpen = document.querySelectorAll(".is-open");
+      accordionTitlesWithIsOpen.forEach((accordionTitleWithIsOpen) => {
+        accordionTitleWithIsOpen.classList.remove("is-open");
+      });
+      accordionTitle.classList.add("is-open");
+    }
+  });
+});
